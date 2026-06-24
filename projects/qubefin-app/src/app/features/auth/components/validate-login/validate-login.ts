0c983a834e23d067464ff1d0fdbb13b65a658680
ayan2 <ayan2@AYAN-PLAYGROUND>
2026-06-24 06:50:40 +0530

    login page changed

--- projects/qubefin-app/src/app/features/auth/components/validate-login/validate-login.ts
+++ projects/qubefin-app/src/app/features/auth/components/validate-login/validate-login.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormField, form } from '@angular/forms/signals';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { ValidateLoginModel } from '../../models/validate-login-model';
 
 @Component({
 	selector: 'qfin-validate-login',
-	imports: [],
+	imports: [FormField, MatFormFieldModule, MatIconModule, MatInputModule],
 	templateUrl: './validate-login.html'
 })
 export class ValidateLogin {
+	hidePassword = signal(true);
 
+	protected readonly loginModel = signal<ValidateLoginModel>({
+		username: '',
+		password: ''
+	});
+
+	protected readonly loginForm = form(this.loginModel);
+
+	submit() {
+
+		// if (this.loginForm.invalid()) {
+		// 	this.loginForm.markAllAsTouched();
+		// 	return;
+		// }
+
+		console.log(this.loginForm().value());
+
+	}
 }
+
---
+++ projects/qubefin-app/src/app/features/auth/models/validate-login-model.ts
@@ -0,0 +1,4 @@
+export interface ValidateLoginModel {
+  username: string;
+  password: string;
+}
