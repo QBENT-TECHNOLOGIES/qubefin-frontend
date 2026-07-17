d12efadb3355050953dbda2bf680b56626bb1500
RKMondal98 <129171887+RKMondal98@users.noreply.github.com>
2026-07-17 16:09:17 +0530

    changes

--- projects/qubefin-app/src/app/layouts/secure/breadcrumb/breadcrumb.ts
+++ projects/qubefin-app/src/app/layouts/secure/breadcrumb/breadcrumb.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component,computed, inject } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
-import { LucideHouse, LucideChevronRight } from '@lucide/angular';
+import { LucideDynamicIcon } from '@lucide/angular';
+import { RouteDataService } from 'qubefin-core';
+import { APP_ICONS_MAP } from '../../../lucide-icons';
 
 @Component({
   selector: 'qfin-breadcrumb',
-  imports: [MatIconModule, LucideHouse,LucideChevronRight ],
+  imports: [MatIconModule, LucideDynamicIcon ],
   templateUrl: './breadcrumb.html'
 })
 export class Breadcrumb {
-
+  routeDataService = inject(RouteDataService);
+  readonly iconMap = APP_ICONS_MAP;
+  pageTitle = computed(() => this.routeDataService.routeData().title);
 }
