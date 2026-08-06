f76d51929e4fdf1055c3a69cd845ae45fe69fb2a
Sujan Ghosh <sujan.ghosh@qbent.in>
2026-08-06 14:13:52 +0530

    changes

--- projects/qubefin-app/src/app/features/hrms/components/attendance-regularizations/attendance-regularization-apply/attendance-regularization-apply.ts
+++ projects/qubefin-app/src/app/features/hrms/components/attendance-regularizations/attendance-regularization-apply/attendance-regularization-apply.ts
@@ -7,17 +7,12 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { LucideDynamicIcon } from '@lucide/angular';
 import { DateAdapter, provideNativeDateAdapter } from '@angular/material/core';
-import Swal from 'sweetalert2';
-import { form, required, Schema, schema } from '@angular/forms/signals';
+import { form, FormField, required, Schema, schema } from '@angular/forms/signals';
 import { AttendanceService } from '../../../services/attendance-service';
 import { MatIconModule } from '@angular/material/icon';
 import { AttendanceRegularizationsStore } from '../../../stores/attendance-regularizations-store';
-
-export interface IRegularizationForm {
-  regularizationType: string;
-  reason: string;
-  regularizationDates: Date[];
-}
+import { IRegularizationForm } from '../../../models/attendance-regularization';
+import { AlertService } from 'qubefin-core';
 
 @Component({
   selector: 'qfin-attendance-regularization-apply',
@@ -30,12 +25,14 @@ export interface IRegularizationForm {
     MatDatepickerModule,
     LucideDynamicIcon,
     MatIconModule,
+    FormField,
   ],
   providers: [provideNativeDateAdapter(), DatePipe],
   templateUrl: './attendance-regularization-apply.html',
   styles: ``,
 })
 export class AttendanceRegularizationApply {
+  private readonly alertService = inject(AlertService);
   private readonly attendanceService = inject(AttendanceService);
   private readonly dateAdapter = inject(DateAdapter<Date>);
   private readonly datePipe = inject(DatePipe);
@@ -81,10 +78,6 @@ export class AttendanceRegularizationApply {
 
       return { ...m, regularizationType, regularizationDates: newDates, reason: updatedReason };
     });
-  }
-
-  updateReason(reason: string) {
-    this.formModel.update((m) => ({ ...m, reason }));
   }
 
   onDateSelected(event: any, inputElement: HTMLInputElement) {
@@ -175,54 +168,22 @@ export class AttendanceRegularizationApply {
       formData.append('attachment', this.selectedFile() as Blob);
     }
 
-    // Swal.fire({
-    //   title: 'Are you sure?',
-    //   text: `You are applying regularization.`,
-    //   icon: 'question',
-    //   showCancelButton: true,
-    //   confirmButtonColor: '#3085d6',
-    //   cancelButtonColor: '#d33',
-    //   confirmButtonText: 'Yes, Apply',
-    // }).then((result) => {
-    //   if (result.isConfirmed) {
-    this.attendanceService.applyRegularization(formData).subscribe((resp: any) => {
-      Swal.fire({
-        html: `
-                  <div class="alert-icon-wrapper success">
-                    <div class="confetti c1"></div>
-                    <div class="confetti c2"></div>
-                    <div class="confetti c3"></div>
-                    <div class="confetti c4"></div>
-                    <div class="confetti c5"></div>
-                    <div class="circle-outer success-outer">
-                      <div class="circle-inner success-inner">
-                        <svg width="36" height="36" viewBox="0 0 24 24" fill="none">
-                          <path d="M5 13l4 4L19 7" stroke="white" stroke-width="3"
-                            stroke-linecap="round" stroke-linejoin="round"/>
-                        </svg>
-                      </div>
-                    </div>
-                  </div>
-                  <h2 class="swal-title">Success!</h2>
-                  <p class="swal-text">${resp.message}</p>
-              <svg viewBox="0 0 1440 320" class="btm-svg">
-                <path fill="#2fae59" fill-opacity="0.1" d="M0,96L34.3,122.7C68.6,149,137,203,206,240C274.3,277,343,299,411,298.7C480,299,549,277,617,272C685.7,267,754,277,823,272C891.4,267,960,245,1029,213.3C1097.1,181,1166,139,1234,117.3C1302.9,96,1371,96,1406,96L1440,96L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z"></path>
-              </svg>
-                `,
-        showConfirmButton: true,
-        confirmButtonText: 'Ok',
-        reverseButtons: true,
-        buttonsStyling: false,
-        customClass: {
-          popup: 'swal-popup swal-popup-success',
-          confirmButton: 'swal-btn swal-btn-success',
-          actions: 'swal-actions',
-        },
-      }).then(() => {
-        this.save.emit();
+    this.alertService
+      .confirm('Confirmation', 'Do you want to apply regularization?', 'Yes', 'No')
+      .then((result: any) => {
+        if (result.isConfirmed) {
+          this.attendanceService.applyRegularization(formData).subscribe({
+            next: (resp: any) => {
+              this.alertService.success('Success', resp.message).then(() => {
+                this.store.refreshList();
+                this.save.emit();
+              });
+            },
+            error: (err: any) => {
+              this.alertService.error('Failed', err.error.message);
+            },
+          });
+        }
       });
-    });
-    //   }
-    // });
   }
 }
