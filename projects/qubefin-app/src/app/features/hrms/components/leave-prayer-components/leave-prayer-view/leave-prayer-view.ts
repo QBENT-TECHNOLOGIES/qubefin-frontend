26be2753600c04272579439486af1def9ea6de47
Sujan Ghosh <sujan.ghosh@qbent.in>
2026-08-13 15:07:56 +0530

    ok

--- projects/qubefin-app/src/app/features/hrms/components/leave-prayer-components/leave-prayer-view/leave-prayer-view.ts
+++ projects/qubefin-app/src/app/features/hrms/components/leave-prayer-components/leave-prayer-view/leave-prayer-view.ts
@@ -42,22 +42,11 @@ export class LeavePrayerView {
   readonly leavePrayer = this.leavePrayerStore.leavePrayer;
   readonly loading = this.leavePrayerStore.leavePrayerLoading;
   readonly error = this.leavePrayerStore.leavePrayerError;
-  // readonly showError = signal(false);
 
-  // rejectedReasonControl = new FormControl('', {
-  //   nonNullable: true,
-  //   validators: [],
-  // });
   constructor() {
     effect(() => {
       this.leavePrayerStore.setLeaveRequestId(this.leavePrayerId());
     });
-    // effect(() => {
-    //   const req = this.leaveRequest(); // assuming `request` is a signal/input — adjust if it's a plain @Input()
-    //   if (req?.isCancellable && req?.rejectedReason) {
-    //     this.cancelReasonControl.setValue(req.rejectedReason);
-    //   }
-    // });
   }
   openDocument(url: any, name: any) {
     if (!url || !name) {
@@ -132,29 +121,12 @@ export class LeavePrayerView {
     }
   }
   protected onAction(action: 'approve' | 'reject' | 'recommend') {
-    // if (action === 'reject') {
-    //   this.rejectedReasonControl.setValidators([Validators.required]);
-    //   this.rejectedReasonControl.updateValueAndValidity();
-    //   const remarks = this.rejectedReasonControl.value.trim();
-
-    //   if (!remarks) {
-    //     this.alertService.warning(null, 'Please enter remarks.');
-    //     this.showError.set(true);
-    //     this.rejectedReasonControl.markAsTouched();
-    //     return;
-    //   }
-    // }
-
-    // this.rejectedReasonControl.clearValidators();
-    // this.rejectedReasonControl.updateValueAndValidity();
-    // this.showError.set(false);
-
     this.alertService
       .confirm(null, `Are you sure you want to ${action} this leave prayer?`, 'Yes', 'No')
       .then((result: any) => {
         if (result.isConfirmed) {
           const payLoad: any = {
-            leavePrayerId: this.leavePrayer()?.id, // Ensure this property name matches your backend DTO
+            leavePrayerId: this.leavePrayer()?.id,
             isApproved: action === 'approve',
             isRejected: action === 'reject',
             rejectedReason: null,
